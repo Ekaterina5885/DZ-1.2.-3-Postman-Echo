9461abcf8e61b23f0eed98aebc32970b45ebb93d
Ekaterina Kolesnikova <1005885@mail.ru>
2021-07-24 12:43:44 +0300

    Update Test V1

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -16,12 +16,7 @@ public class PostmanEchoTest {
                 .post("/post")
         .then()
                 .statusCode(200)
-                .contentType(ContentType.JSON)
                 .body("data", equalTo("some data"))
-                .body("headers.accept", equalTo("*/*"))
-                .body("headers.accept-encoding", equalTo("gzip,deflate"))
-                .body("headers.user-agent", equalTo("Apache-HttpClient/4.5.3 (Java/11.0.11)"))
-                .body("headers.host", equalTo("postman-echo.com"))
         ;
     }
 }
